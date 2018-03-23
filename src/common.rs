26da81b9298b39a2625bd870f1fa1efd8f3e0895
rskupnik <r.skupnik@gmail.com>
2018-03-23 20:30:42 +0100

    Add Loop struct and some tests

--- src/common.rs
+++ src/common.rs
@@ -11,13 +11,42 @@ pub enum Command {
     Unknown
 }
 
-/*impl PartialEq for Command {
-    fn eq(&self, other: &Command) -> bool {
-        match (self, other) {
-            (&Command::Input(ref a), &Command::Input(ref b)) => a == b,
-            (&Command::Input(ref a), &Command) => false,
-            (&Command, &Command::Input(ref a)) => false,
-            _ => false
-        }
+pub struct Loop {
+    start_pos: usize,
+    end_pos: usize,
+    counter_ptr: usize
+}
+
+impl Loop {
+    
+    pub fn new(pos: usize) -> Loop {
+        Loop { start_pos: pos, end_pos: 0, counter_ptr: 0 }
     }
-}*/
+
+    pub fn uninitialized() -> Loop {
+        Loop { start_pos: 0, end_pos: 0, counter_ptr: 0}
+    }
+
+    pub fn start_pos(&self) -> &usize {
+        &self.start_pos
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_construct_new_loop() {
+        let lp = Loop::new(10);
+
+        assert_eq!(10, *lp.start_pos());
+    }
+
+    #[test]
+    fn should_construct_uninitialized_loop() {
+        let lp = Loop::uninitialized();
+        
+        assert_eq!(0, *lp.start_pos());
+    }
+}
